a647ee4a617f6cf13de2d491d79421a018f3a028
star <>
2018-10-08 15:32:15 +0800

    删除多余的公共方法

--- src/assets/js/utils.js
+++ src/assets/js/utils.js
@@ -143,28 +143,6 @@ export const secondToString = second => {
     return time
 }
 
-// 毫秒转为时间xxxx/xx/xx xx:xx:xx
-export const msecToTime = msec => {
-    if (!msec) {
-        return msec
-    } else {
-        let time = new Date(msec)
-        return (
-            time.getFullYear() +
-            '/' +
-            (time.getMonth() + 1 < 10 ? '0' + (time.getMonth() + 1) : time.getMonth() + 1) +
-            '/' +
-            (time.getDate() < 10 ? '0' + time.getDate() : time.getDate()) +
-            ' ' +
-            (time.getHours() < 10 ? '0' + time.getHours() : time.getHours()) +
-            ':' +
-            (time.getMinutes() < 10 ? '0' + time.getMinutes() : time.getMinutes()) +
-            ':' +
-            (time.getSeconds() < 10 ? '0' + time.getSeconds() : time.getSeconds())
-        )
-    }
-}
-
 // 格式化日期
 export const formatDate = (date, fmt) => {
     if (!date) {
